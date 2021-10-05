7bcf1282e0c4224a3918affcb6f0143bce2dc8b7
harchebnikovaay <harchebnikova99@mail.ru>
2021-10-05 13:41:02 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const words = Object.keys(require('./words_dictionary.json'));
+
+app.get('/', function (req, res) {
+    const filteredWords = words.filter(e => e.indexOf(req.query.q) === 0);
+    res.json(filteredWords);
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is live at http://localhost:${PORT}`));
