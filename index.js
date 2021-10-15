acc719de9061f841e0f68aefe780316983e0e4f5
harchebnikovaay <harchebnikova99@mail.ru>
2021-10-15 16:15:06 +0700

    some changes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const words = Object.keys(require('./words_dictionary.json'));
-// const words = Object(require('./test_dictionary.json'));
+// const words = Object.keys(require('./words_dictionary.json'));
+const words = Object(require('./test_dictionary.json'));
+const translate = require('@iamtraction/google-translate');
+let translation;
+
+
 
 app.use(
     cors({
@@ -12,19 +16,22 @@ app.use(
     })
 );
 
+app.get('/', async function (req, response) {
+    await translate(req.query.q, {from:"en", to:"ru"}).then(res => {
+        translation = res.text;
+        console.log(translation, res.text);
+    })
+    console.log(translation);
+    response.json(translation);
+})
+
 // app.get('/', function (req, res) {
 //     const filteredWords = words
-//         .filter(e => e.name.indexOf(req.query.q) === 0)
+//         .filter(e => e.indexOf(req.query.q) === 0)
 //         .splice(0,10);
 //     res.json(filteredWords);
 // })
 
-app.get('/', function (req, res) {
-    const filteredWords = words
-        .filter(e => e.indexOf(req.query.q) === 0)
-        .splice(0,10);
-    res.json(filteredWords);
-})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server is live at http://localhost:${PORT}`));
